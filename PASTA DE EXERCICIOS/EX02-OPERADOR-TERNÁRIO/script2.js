8d61d699bc8228a615534ea5e747a27a2236cfa8
Giovanna Xavier <giovannacostaxavier@gmail.com>
2025-08-18 17:35:06 +0100

    exercicio

---
+++ PASTA DE EXERCICIOS/EX02-OPERADOR-TERNÁRIO/script2.js
@@ -0,0 +1,15 @@
+const num = 1
+
+let res = num%2==0 ? "Par" : "Impar"
+//console.log(res);
+
+const idade = 18
+
+let verificar = idade >= 18 ? "Maior de idade" : "Menor de idade"
+
+//console.log(verificar);
+
+const firstName = "Giovanna"
+const lastName = "Xavier"
+
+let nomeCompleto = 
