15fe2e9d26dc366b780ec19bba47ffff8ff845c6
Giovanna Xavier <giovannacostaxavier@gmail.com>
2025-08-26 18:07:13 +0100

    novo exercicio

---
+++ PASTA DE EXERCICIOS/EX05-LOOP-FOR/script.js
@@ -0,0 +1,27 @@
+for(let i=0; i < 100; i++){
+    //console.log(i);
+    
+}
+
+for(let i=50; i>0;i-=2){
+    //console.log(i);
+    
+}
+let soma = 0
+for(let i=0; i<=50;i++){
+    soma += i
+    //console.log(`A soma de todos os números é ${soma}`);
+    
+}
+
+for(let i=0; i<=100; i++){
+    if(i%2===0){
+        //console.log(i);
+        
+    }
+}
+
+for(let i=0; i<10;i*=2){
+    console.log(i);
+    
+}
